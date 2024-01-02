ab13920c3e3c96617edc8e92d38ab5c84df40a78
Pedro Gabriel <58262161+pedrold10@users.noreply.github.com>
2024-01-02 17:56:23 -0300

    Inicializando backend

---
+++ produtobackend/src/main/java/com/nunessports/produtobackend/ProdutobackendApplication.java
@@ -0,0 +1,13 @@
+package com.nunessports.produtobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutobackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutobackendApplication.class, args);
+	}
+
+}
---
+++ produtobackend/src/test/java/com/nunessports/produtobackend/ProdutobackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nunessports.produtobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutobackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
